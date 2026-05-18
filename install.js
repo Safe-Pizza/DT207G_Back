325ab01a23b63060e3dbebf162c55e6a84b76c13
Hanna Lindkvist <hali2507@student.miun.se>
2026-05-18 16:39:29 +0200

    skriver createTables, skapar tabell i databas

--- install.js
+++ install.js
@@ -18,5 +18,24 @@ client.connect((err) => {
         console.log("Conntection error: " + err);
     } else {
         console.log("Connected to database");
+        createTables();
     }
 })
+
+async function createTables() {
+    try {
+        const res = await client.query(`
+            CREATE TABLE IF NOT EXISTS user_cred (
+            id SERIAL PRIMARY KEY,
+            email TEXT NOT NULL UNIQUE,
+            name TEXT NOT NULL,
+            password TEXT NOT NULL,
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP)
+            `)
+            console.log(res)
+    } catch (err) {
+        console.log(err)
+    } finally {
+        await client.end()
+    }
+}
